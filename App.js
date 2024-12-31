bd0907ed5f9f2b3e2c204fa240057e40167e2b30
DuyAnh2074 <duya2074@gmail.com>
2024-12-31 21:24:41 +0700

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+const PhoneNumberInput = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Đăng nhập</Text>
+      <Text style={styles.label}>Nhập số điện thoại</Text>
+      <Text style={styles.description}>
+        Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản tại OneHousing Pro
+      </Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="phone-pad"
+        placeholder="Nhập số điện thoại của bạn"
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+      />
+      <TouchableOpacity
+        style={[styles.button, phoneNumber ? styles.buttonEnabled : styles.buttonDisabled]}
+        disabled={!phoneNumber}
+      >
+        <Text style={styles.buttonText}>Tiếp tục</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
+    justifyContent: 'flex-start', 
+    alignItems: 'flex-start',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 80 ,
+    paddingTop: 40,
+    paddingBottom: 20,
+    borderBottomWidth: 0.5,
+    width: '100%',
+  },
+  label: {
+    fontSize: 22,
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 20,
+  },
+  input: {
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderColor: '#CCCCCC',
+    padding: 10,
+    marginBottom: 230,
+    borderRadius: 5,
+    width: '100%',
+  },
+  button: {
+    padding: 15,
+    borderRadius: 5,
+    
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+  },
+  buttonEnabled: {
+    backgroundColor: '#007BFF',
+  },
+  buttonDisabled: {
+    backgroundColor: '#CCCCCC',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+   
   },
 });
+
+export default PhoneNumberInput;
