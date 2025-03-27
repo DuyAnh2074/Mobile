58942764cfe832350c76b3c91c479cc7f90210a1
DuyAnh2074 <duya2074@gmail.com>
2025-03-27 13:02:07 +0700

    nop thuc hanh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './payment';
+import App from './Nectar';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
